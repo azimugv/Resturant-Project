adbb765a32d2dfb34ce890f5e5aa19bc90ba4bed
Azim <azimugv@gmail.com>
2022-09-22 06:54:30 +0600

    Restaurant Contact From And Footer Implemented

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='footer'>
+            <div className='container'>
+                <div className='row justify-content-center'>
+                    <div className='col-auto'>
+                        <p>AAA Restaurant &copy; Copyright 2020</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
